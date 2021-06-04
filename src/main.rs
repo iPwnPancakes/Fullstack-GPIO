fa4c06bfff67e864b85d2ffb400eb20755182eb9
iPwnPancakes <Daniel@DanielBuenrrostro.com>
2021-06-04 15:52:43 -0500

    Deprecated parse_pin_number fn

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,39 @@
 extern crate sysfs_gpio;
 
 use std::env;
+use std::process;
 use sysfs_gpio::{Direction, Pin};
 
 fn main() {
-    let pin_num_arg = env::args().nth(1).expect("Expected a first argument");
-    let direction_arg = env::args().nth(2).expect("Expected a second argument");
-
-    let pin_num = parse_pin_number(pin_num_arg);
-    let direction = parse_direction(direction_arg);
-
-    println!(
-        "Pin: {} Direction: {}",
-        pin_num,
-        match direction {
-            Direction::In => "in",
-            Direction::Out => "out",
-            Direction::High => "high",
-            Direction::Low => "low",
+    let pin_num_arg = match env::args().nth(1) {
+        Some(s) => s,
+        None => {
+            println!("Argument 1 must be specified");
+            process::exit(1);
         }
-    );
+    };
+    let direction_arg = match env::args().nth(2) {
+        Some(s) => s,
+        None => {
+            println!("Argument 2 must be specified");
+            process::exit(1);
+        }
+    };
+
+    let pin_num = match pin_num_arg.parse::<u64>() {
+        Ok(n) => n,
+        Err(_) => {
+            process::exit(1);
+        }
+    };
+    let direction = match parse_direction(direction_arg) {
+        Ok(dir) => dir,
+        Err(e) => {
+            println!("Error: {}", e);
+            process::exit(1);
+        }
+    };
+
     let my_pin = Pin::new(pin_num);
 
     let result = my_pin.with_exported(|| {
@@ -28,23 +42,23 @@ fn main() {
     });
 
     match result {
-        Ok(_) => println!("Done"),
-        Err(err) => println!("Unhandled Error: {}", err),
+        Ok(_) => {
+            println!("Done");
+            process::exit(0);
+        }
+        Err(err) => {
+            println!("Unhandled Error: {}", err);
+            process::exit(1);
+        }
     }
 }
 
-fn parse_pin_number(str: String) -> u64 {
-    let pin_number: u64 = str.parse().unwrap();
-
-    return pin_number;
-}
-
-fn parse_direction(str: String) -> Direction {
+fn parse_direction(str: String) -> Result<Direction, String> {
     if str == "in" {
-        return Direction::In;
+        return Ok(Direction::In);
     } else if str == "out" {
-        return Direction::Out;
+        return Ok(Direction::Out);
     } else {
-        panic!("Direction must match in or out");
+        return Err("Invalid Direction: Must be either in or out".to_string());
     }
 }
