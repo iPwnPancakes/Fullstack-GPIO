032af47649071122f63d12f8ca01c81535786457
iPwnPancakes <Daniel@DanielBuenrrostro.com>
2021-06-09 08:52:38 -0500

    Implemented VacuumDeviceDriver methods

--- packages/backend/app/Services/DeviceCommunicationService/Drivers/VacuumDeviceDriver.php
+++ packages/backend/app/Services/DeviceCommunicationService/Drivers/VacuumDeviceDriver.php
@@ -4,9 +4,14 @@ namespace App\Services\DeviceCommunicationService\Drivers;
 
 use App\Core\Result;
 use App\Models\Vacuum;
+use Exception;
+use GuzzleHttp\Exception\ConnectException;
+use Illuminate\Support\Facades\Http;
 
 class VacuumDeviceDriver implements IDriverConnection
 {
+    private $vacuum;
+
     public function __construct(Vacuum $vacuum)
     {
         $this->vacuum = $vacuum;
@@ -14,16 +19,70 @@ class VacuumDeviceDriver implements IDriverConnection
 
     public function ping(): Result
     {
-        return Result::fail('Not implemented');
+        try {
+            $response = Http::get($this->vacuum->public_ip . ':' . $this->vacuum->port . '/ping');
+
+            if (!$response->ok()) {
+                return Result::fail('Response received from device not ok');
+            }
+
+            $json = $response->json();
+
+            if ($json === null) {
+                return Result::fail('Invalid response from device');
+            }
+
+            return Result::ok();
+        } catch (ConnectException $e) {
+            return Result::fail('Could not connect to device');
+        } catch (Exception $e) {
+            return Result::fail('Failure to communicate with vacuum device');
+        }
     }
 
     public function setPower(bool $power): Result
     {
-        return Result::fail('Not implemented');
+        try {
+            $response = Http::get($this->vacuum->public_ip . ':' . $this->vacuum->port . '/setPower?power=' . $power);
+
+            if (!$response->ok()) {
+                return Result::fail('Response received from device not ok');
+            }
+
+            $json = $response->json();
+
+            if ($json === null) {
+                return Result::fail('Invalid response from device');
+            }
+
+            return Result::ok();
+        } catch (ConnectException $e) {
+            return Result::fail('Could not connect to device');
+        } catch (Exception $e) {
+            return Result::fail('Failure to communicate with vacuum device');
+        }
     }
 
     public function getPower(): Result
     {
-        return Result::fail('Not implemented');
+        try {
+            $response = Http::get($this->vacuum->public_ip . ':' . $this->vacuum->port . '/getPower');
+
+            if (!$response->ok()) {
+                return Result::fail('Response received from device not ok');
+            }
+
+            $json = $response->json();
+
+            if ($json === null) {
+                return Result::fail('Invalid response from device');
+            }
+
+            return Result::ok();
+        } catch (ConnectException $e) {
+            return Result::fail('Could not connect to device');
+        } catch (Exception $e) {
+            return Result::fail('Failure to communicate with vacuum device');
+        }
     }
 }
