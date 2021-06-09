eeecf7c5c7ac708ed096bf05539538430c1b1b4f
iPwnPancakes <Daniel@DanielBuenrrostro.com>
2021-06-09 10:25:25 -0500

    Removed unnecessary default Laravel routes/resources/code

--- packages/backend/app/Providers/RouteServiceProvider.php
+++ packages/backend/app/Providers/RouteServiceProvider.php
@@ -42,10 +42,6 @@ class RouteServiceProvider extends ServiceProvider
                 ->middleware('api')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
-
-            Route::middleware('web')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/web.php'));
         });
     }
 
@@ -57,7 +53,7 @@ class RouteServiceProvider extends ServiceProvider
     protected function configureRateLimiting()
     {
         RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
+            return Limit::perMinute(30)->by(optional($request->user())->id ?: $request->ip());
         });
     }
 }
