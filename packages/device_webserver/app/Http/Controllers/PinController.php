b1a65f5e73c7f683eb739c51ba1cc649953225ba
iPwnPancakes <Daniel@DanielBuenrrostro.com>
2021-06-19 21:59:31 -0500

    Implemented Http PinController

---
+++ packages/device_webserver/app/Http/Controllers/PinController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\UseCases\Pins\GetPinState\GetPinState;
+use App\UseCases\Pins\GetPinState\GetPinStateDTO;
+use App\UseCases\Pins\GetPinState\GetPinStateResponseDTO;
+use App\UseCases\Pins\SetPinPower\SetPinPower;
+use App\UseCases\Pins\SetPinPower\SetPinPowerDTO;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class PinController extends Controller
+{
+    private $setPinPowerCommand;
+    private $getPinStateQuery;
+
+    public function __construct(SetPinPower $setPinPowerCommand, GetPinState $getPinStateQuery)
+    {
+        $this->setPinPowerCommand = $setPinPowerCommand;
+        $this->getPinStateQuery = $getPinStateQuery;
+    }
+
+    public function getPinState(Request $request): JsonResponse
+    {
+        $request->validate([
+            'pin_number' => 'required|numeric'
+        ]);
+
+        $dto = new GetPinStateDTO($request->all());
+
+        $result = $this->getPinStateQuery->execute($dto);
+
+        if ($result->isFailure()) {
+            return response()->json([
+                'errors' => $result->getErrors()
+            ], 500);
+        }
+
+        /** @var GetPinStateResponseDTO */
+        $response = $result->getValue();
+
+        return response()->json($response->toArray());
+    }
+
+    public function setPinPower(Request $request): JsonResponse
+    {
+        $request->validate([
+            'pin_number' => 'required|numeric',
+            'power' => 'required|boolean'
+        ]);
+
+        $dto = new SetPinPowerDTO($request->all());
+
+        $result = $this->setPinPowerCommand->execute($dto);
+
+        if ($result->isFailure()) {
+            return response()->json([
+                'errors' => $result->getErrors()
+            ], 500);
+        }
+
+        return response()->json(['message' => 'ok']);
+    }
+}
--- packages/device_webserver/routes/web.php
+++ packages/device_webserver/routes/web.php
@@ -14,3 +14,6 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->post('/pin/getStatus', [\App\Http\Controllers\PinController::class, 'getPinState']);
+$router->post('pin/setPower', [\App\Http\Controllers\PinController::class, 'setPinPower']);
