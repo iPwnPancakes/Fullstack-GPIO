795c0cf0c6c6112eb0b416e3ec083202d0558444
iPwnPancakes <Daniel@DanielBuenrrostro.com>
2021-06-21 19:01:15 -0500

    Fixed event listener mapping bug

--- packages/backend/app/Providers/EventServiceProvider.php
+++ packages/backend/app/Providers/EventServiceProvider.php
@@ -14,7 +14,7 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        VacuumTimedOut::class => MarkVacuumDisconnected::class
+        VacuumTimedOut::class => [MarkVacuumDisconnected::class]
     ];
 
     /**
