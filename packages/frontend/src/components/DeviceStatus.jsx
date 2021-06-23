c0030e9a7566f7b8170afbb3b4f40da9c0e50efd
iPwnPancakes <Daniel@DanielBuenrrostro.com>
2021-06-23 10:45:16 -0500

    Handled case where device status properties were null

--- packages/frontend/src/components/DeviceStatus.jsx
+++ packages/frontend/src/components/DeviceStatus.jsx
@@ -13,9 +13,11 @@ export function DeviceStatus() {
     return (
         <Box>
             <Text fontSize={'xl'}>Device Status</Text>
-            <Text>Online: {deviceState.online.toString()}</Text>
-            <Text>Vacuum On: {deviceState.is_on.toString()}</Text>
-            <Text>Last Communication: {deviceState.last_communication_time.toString()}</Text>
+            <Text>Online: {deviceState.online ? 'True' : 'False'}</Text>
+            <Text>Vacuum On: {deviceState.is_on ? 'True' : 'False'}</Text>
+            <Text>
+                Last Communication: {deviceState.last_communication_time ? deviceState.last_communication_time : 'N/A'}
+            </Text>
         </Box>
     )
 }
