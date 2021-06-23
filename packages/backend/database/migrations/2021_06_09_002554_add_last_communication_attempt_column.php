236d066ca4927b6106694fec55a193d6591d2e73
iPwnPancakes <Daniel@DanielBuenrrostro.com>
2021-06-23 09:32:47 -0500

    Fixed last_communication_at column type in migration

--- packages/backend/database/migrations/2021_06_09_002554_add_last_communication_attempt_column.php
+++ packages/backend/database/migrations/2021_06_09_002554_add_last_communication_attempt_column.php
@@ -14,7 +14,7 @@ class AddLastCommunicationAttemptColumn extends Migration
     public function up()
     {
         Schema::table('vacuum', function (Blueprint $table) {
-            $table->timestamp('last_communication_attempt_at');
+            $table->dateTime('last_communication_attempt_at');
         });
     }
 
