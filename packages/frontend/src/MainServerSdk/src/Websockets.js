5d9cd5a473ceaae1ef395a0068295e4c688d6158
iPwnPancakes <Daniel@DanielBuenrrostro.com>
2021-08-04 17:54:10 -0500

    Added separate env var for Websocket server URL

--- packages/frontend/src/MainServerSdk/src/Websockets.js
+++ packages/frontend/src/MainServerSdk/src/Websockets.js
@@ -2,7 +2,7 @@ import Echo from "laravel-echo";
 import Pusher from "pusher-js";
 
 const client = new Pusher(process.env.REACT_APP_WEBSOCKET_SERVER_KEY, {
-    wsHost: process.env.REACT_APP_MAIN_SERVER_URL,
+    wsHost: process.env.REACT_APP_WEBSOCKET_SERVER_URL,
     wsPort: process.env.REACT_APP_WEBSOCKET_SERVER_PORT,
     cluster: process.env.PUSHER_APP_CLUSTER,
     forceTLS: false
